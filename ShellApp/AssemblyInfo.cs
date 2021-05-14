ada173f0526d8d2cf1cbc1e6d71b66e63789e08a
Robert Sundström <robert.sundstrom@outlook.com>
2021-05-14 12:24:08 +0200

    Use Font Awesome glyph for Plus symbol #1

--- ShellApp/AssemblyInfo.cs
+++ ShellApp/AssemblyInfo.cs
@@ -1,3 +1,8 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("FontAwesomeIcons.otf", Alias = "FA Icons")]
+[assembly: ExportFont("FontAwesomeRegular.otf", Alias = "FA Regular")]
+[assembly: ExportFont("FontAwesomeSolid.otf", Alias = "FA Solid")]
