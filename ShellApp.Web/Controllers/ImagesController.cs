432428c12a37f170d09af16a2adca474019abc08
Robert Sundström <robert.sundstrom@outlook.com>
2021-05-15 10:00:29 +0200

    Add Azure blob storage support

---
+++ ShellApp.Web/Controllers/ImagesController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShellApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly BlobContainerClient blobContainerClient;
+
+        public ImagesController(BlobContainerClient blobContainerClient)
+        {
+            this.blobContainerClient = blobContainerClient;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Get(string id)
+        {
+            var client = blobContainerClient.GetBlobClient(id);
+            
+            Console.WriteLine(client.GenerateSasUri(Azure.Storage.Sas.BlobSasPermissions.Read, DateTimeOffset.MaxValue));
+            var result = await client.DownloadContentAsync();
+            return File(result.Value.Content.ToStream(), "text/text");
+        }
+    }
+}
--- ShellApp.Web/Startup.cs
+++ ShellApp.Web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +32,11 @@ namespace ShellApp.Web
             services.AddDbContext<ApplicationDataContext>(c => c.UseSqlServer(Configuration.GetConnectionString("mssql")));
 
             services.AddTransient<IItemRepository, ItemRepository>();
+
+            BlobClientOptions options = new BlobClientOptions(BlobClientOptions.ServiceVersion.V2019_07_07);
+
+            services.AddScoped(sp => new BlobContainerClient(
+                "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;", "images", options));
 
             services.AddOpenApiDocument();
         }
