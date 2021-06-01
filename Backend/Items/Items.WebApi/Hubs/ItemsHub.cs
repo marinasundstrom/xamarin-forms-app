04d1bba5fc4c7baed5b2c9287b485a8f95a5b834
Robert Sundström <robert.sundstrom@outlook.com>
2021-06-01 09:49:18 +0200

    Retrieve user in ItemsHub

--- Backend/Items/Items.WebApi/Hubs/ItemsHub.cs
+++ Backend/Items/Items.WebApi/Hubs/ItemsHub.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using ShellApp.Items.Application.Common.Interfaces;
@@ -8,6 +9,16 @@ namespace ShellApp.Items.WebApi.Hubs
     [Authorize(LocalApi.AuthenticationScheme)]
     public class ItemsHub : Hub<IItemsNotificationClient>
     {
+        public override Task OnConnectedAsync()
+        {
+            var user = Context.User;
 
+            return Task.CompletedTask;
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
